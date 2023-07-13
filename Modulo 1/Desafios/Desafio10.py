edae08e8a233bc9a15f1d491e8dae111b1dc180a
Rafael Braz <98630687+Rarazc@users.noreply.github.com>
2023-07-13 11:48:43 -0300

    .

--- Modulo 1/Desafios/Desafio10.py
+++ Modulo 1/Desafios/Desafio10.py
@@ -1,3 +1,3 @@
-dinheiro=int(input("Digite quanto dinheiro você tem: "))
+dinheiro=float(input("Digite quanto dinheiro você tem: "))
 
 print(f"Com R$:{dinheiro:.2f} você consegue comprar $:{dinheiro/3.27:.2f} dolares")
