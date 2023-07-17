4021c0bb3d92fb5c1fb1a3294c57ea9dd1ead228
Rafael Braz <98630687+Rarazc@users.noreply.github.com>
2023-07-17 10:34:12 -0300

    .

--- Modulo 1/Desafios/Desafio17.py
+++ Modulo 1/Desafios/Desafio17.py
@@ -3,4 +3,4 @@ import math
 oposto = float(input("Digite o valor do cateto oposto: "))
 adjacente = float(input("Digite o valor do cateto adjacente: "))
 
-print(f"A hipotenusa resultante é: {math.hypot(oposto,adjacente)}")
+print(f"A hipotenusa resultante é: {math.hypot(oposto,adjacente):.2f}")
--- Modulo 1/Desafios/Desafio18.py
+++ Modulo 1/Desafios/Desafio18.py
@@ -2,4 +2,4 @@ import math
 
 angulo = int(input("Digite o valor do angulo: "))
 
-print(f"seno: {math.asin(angulo):.2f} \ncosseno: {math.acos(angulo):.2f} \ntangente: {math.atan(angulo):.2f}")
+print(f"seno: {math.sin(math.radians(angulo)):.2f} \ncosseno: {math.cos(math.radians(angulo)):.2f} \ntangente: {math.tan(math.radians(angulo)):.2f}")
